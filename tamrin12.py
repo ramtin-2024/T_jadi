41e67249f11148001da3cdb7ef9ac46f9b37441e
ramtin <ramtin.windows.11pro@gmail.com>
2026-05-08 13:16:02 +0330

    Practice Series 12 Start Function writing with Python First Function

---
+++ tamrin12.py
@@ -0,0 +1,10 @@
+# تابعی به نام 
+# hello_world
+# بنویسید که هیچ ورودی‌ای دریافت نکند و فقط متن زیر را چاپ کند.
+# "Hello, World!"
+
+def hello_world():
+    print("Hello, World!")
+ 
+
+hello_world()
